85ef38cc54f18c59b74c550983239f7ef036f6ff
Nikita Balashov <nikita.89534588306@gmail.com>
2023-02-06 18:14:29 +0500

    a

---
+++ app/templates/components/header.php
@@ -0,0 +1,37 @@
+<header  class="header">
+	<div class="container">
+		<div class="row">
+			<div class="logo h2 col-12 col-sm-2 ">
+				My site
+			</div>
+
+			<ul class="nav col-12 p-0 col-sm-10  d-block d-sm-flex flex-sm-row justify-content-end">
+				<li class="nav-item"><a href="#" class="nav-link px-2 text-white">Главная</a></li>
+				<li class="nav-item"><a href="#" class="nav-link px-2 text-white">О нас</a></li>
+				<li class="nav-item"><a href="#" class="nav-link px-2 text-white">Услуги</a></li>
+				<li class="d-none d-sm-flex nav-item user-actions ">
+
+					<div style="width:100%;"class=" d-none d-sm-block ">
+						<i class='fa fa-user d-none d-sm-inline' style=" padding-right:8px; display:inline"></i>
+						<a href="#" class=" d-none d-sm-inline nav-link px-2 text-white">Кабинет</a>
+					</div>
+					
+					<ul class='nav user-account'>
+						<li class="nav-item"><a class="nav-link text-white" href='app/admin/admin.php'>Админ панель</a></li>
+						<li class="nav-item"><a style="width:100%"class="nav-link  text-white" href='./app/include/logout.php'>Выход</a></li>
+					</ul>
+
+				</li>
+				<li class="d-block d-sm-none s_user_action nav-item"><a href="#" class="nav-link px-2 text-white">Кабинет</a>
+					<ul class="nav nav-small-users-actions">
+							<li class="nav-item"><a href="#" class="nav-link px-2 text-white">Админ панель</a></li>
+							<li class="nav-item"><a href="#" class="nav-link px-2 text-white">Выход</a></li>
+					</ul>
+				</li>
+				
+			</ul>
+			
+		</div>
+	</div>
+
+</header>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "head.php" ?>
+</head>
+<body>
+	<?php include "app/templates/components/header.php" ?>
+	<?php include "scripts.php" ?>
+</body>
+</html>
+
+
